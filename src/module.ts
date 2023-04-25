26404e41766255a6cd9bd6a1cf5ed80ccf517142
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2023-04-25 17:57:15 +0100

    URLs should be redirected maybe

--- src/module.ts
+++ src/module.ts
@@ -3,4 +3,18 @@ import grafana from './grafana_func';
 
 grafana.loadCss();
 
+// This hack stops drawio from being able to actually redirect us to pages
+// Note the "link" attribute must start with "Request:"
+// document.domain = "localhost";
+window.open = function (open) {
+    return function(url: string | URL | undefined, name: any, features: any) {
+        let target = url!.toString().split("/").at(-1)!;
+        if (target.startsWith("Request:")) {
+            console.log(target.replace("Request:", ""));
+            return null;
+        }
+        return open.call(window, url, name, features);
+    };
+}(window.open);
+
 export { FlowchartCtrl as PanelCtrl };
